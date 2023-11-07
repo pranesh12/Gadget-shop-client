784b9ae81475566ed9afdb645ac9bbfb2022deea
pranesh12 <praneshchakama6@gmail.com>
2023-11-07 20:37:48 +0600

    router problem resolved and admin layout added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {
   RouterProvider,
   Route,
   createRoutesFromElements,
+  Routes,
 } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import ProductDetail from "./Pages/products/ProductDetail";
@@ -12,27 +13,81 @@ import LoginForm from "./Pages/LoginForm/LoginForm";
 import Cart from "./Pages/Cart/Cart";
 import PrivateRoute from "./routes/PrivateRoute";
 import UserOrder from "./Pages/userOrder/UserOrder";
+import AdminLayOut from "./Pages/Admin/AdminLayOut";
+import Admin from "./Pages/Admin/Admin";
 
 const App = () => {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/product/:productId" element={<ProductDetail />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/userorder" element={<UserOrder />} />
-        <Route
-          path="/cart"
-          element={
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/product/:productId",
+          element: <ProductDetail />,
+        },
+        {
+          path: "/login",
+          element: <LoginForm />,
+        },
+        {
+          path: "/register",
+          element: <ProductDetail />,
+        },
+        {
+          path: "/product/:productId",
+          element: <RegisterForm />,
+        },
+        {
+          path: "/userorder",
+          element: <UserOrder />,
+        },
+        {
+          path: "/cart",
+          element: (
             <PrivateRoute>
               <Cart />
             </PrivateRoute>
-          }
-        />
-      </Route>
-    )
-  );
+          ),
+        },
+      ],
+    },
+
+    {
+      path: "/admin",
+      element: <AdminLayOut />,
+      children: [
+        {
+          path: "/admin",
+          element: <Admin />,
+        },
+      ],
+    },
+  ]);
+
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route path="/" element={<Layout />}>
+  //       <Route index element={<Home />} />
+  //       <Route path="/product/:productId" element={<ProductDetail />} />
+  //       <Route path="/login" element={<LoginForm />} />
+  //       <Route path="/register" element={<RegisterForm />} />
+  //       <Route path="/userorder" element={<UserOrder />} />
+  //       <Route
+  //         path="/cart"
+  //         element={
+  //           <PrivateRoute>
+  //             <Cart />
+  //           </PrivateRoute>
+  //         }
+  //       />
+  //     </Route>
+  //   )
+  // );
 
   return (
     <>
