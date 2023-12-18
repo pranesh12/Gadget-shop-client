e880a7d6ee33931530c97bfd23941e751e8bcadd
pranesh12 <praneshchakama6@gmail.com>
2023-12-18 22:36:31 +0600

    url updated

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 // const token = JSON.parse(localStorage.getItem("token"));
-export const url = "https://shop-nest-hs89.onrender.com/";
+// export const url = "https://shop-nest-hs89.onrender.com/";
+export const url = "https://shop-nest1.vercel.app/";
 
 export const authAxios = axios.create({
   baseURL: url,
